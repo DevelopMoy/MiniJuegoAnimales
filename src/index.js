bfe4c2c3f33df295d09215b81e534d0789b5ee8d
moy <fabmoy1866@gmail.com>
2021-03-11 22:25:37 -0600

    Logica del minijuego completada

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import {Minijuego} from "./js/Minijuego";
 import {Animal} from "./js/Animal";
 
+const juegoPrinc = new Minijuego();
+
+document.querySelector("#nuevoJuego").addEventListener("click",function (){
+    juegoPrinc.iniciarNuevoJuego();
+});
--- src/js/Animal.js
+++ src/js/Animal.js
@@ -1,4 +1,9 @@
-
 export class Animal {
 
+    constructor(idAnim) {
+        this.idAnim=idAnim;
+        this.animalImg=`../assets/animales/pics/p${this.idAnim}.png`;
+        this.animalHabit=`../assets/animales/hab/h${this.idAnim}.jpg`;
+        this.animalSound=`../assets/animales/sound/s${this.idAnim}.mp3`;
+    }
 }
